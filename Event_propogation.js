1bdc11e7aec097901d48998a22203b91ea6128be
Aarav Harit <aaravharit418@gmail.com>
2026-05-06 23:46:08 +0530

    Practice

---
+++ Event_propogation.js
@@ -0,0 +1,16 @@
+// EVENT PROPOGATION: is a process that defines how event travels through the DOM tree when an event is triggered (like a click).
+
+                       // It has 3 phases:
+                                // Capturing phase: Event moves from the root(document) down towards the target element.
+                                // Target phase: The event reaches the exact element that was interacted with.
+                                // Bubbling phase: Event then moves back up from the target to the root.
+
+document.getElementById("outer").addEventListener("click", ()=>{
+    alert("Outer Div(Capturing)");
+},true);
+document.getElementById("inner").addEventListener("click", ()=>{
+    alert("Inner Div(Capturing)");
+},true);
+document.getElementById("btn").addEventListener("click", ()=>{
+    alert("Button Clicked");
+});
