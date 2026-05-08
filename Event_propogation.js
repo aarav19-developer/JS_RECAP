ca08abd7d47f18cf4a1f66e53bd44fcb8e06f63e
Aarav Harit <aaravharit418@gmail.com>
2026-05-08 17:26:25 +0530

    Practice

--- Event_propogation.js
+++ Event_propogation.js
@@ -29,3 +29,22 @@ document.getElementById("myform").addEventListener("submit",function(e){
     e.preventDefault(); // help in stops page reload
     alert("Form submitted with automatically")
 })
+
+
+//-----------------------------------------------------------------------------------------//
+
+// Write an async funciton getTodo. Inside the funciton, use fetch and await to get data for a single "todo" item
+// from the url: https://jsonplaceholder.typicode.com/todos/1. After getting the response, use await again on the response.json() method to parse the data. Finally, log the parsed data.
+
+async function getTodo(){
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+        const data = await response.json();
+        console.log("Todo data:", data)
+
+    } catch(error){
+        console.error("Could not fetch",error)
+    }
+
+}
+getTodo()
